a10b0d5f6f8bad6bcf70e0f8d659d40201610ba2
Anthony Buchanan <ajbuchanjr@gmail.com>
2015-04-01 21:17:47 -0400

    more

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.where(title:'The Hobbit').first_or_create!(title:'The Hobbit', author:'Tolkien', pages:384, price:22.68)
+Book.where(title:'Atlas Shrugged').first_or_create!(title:'Atlas Shrugged', author:'Ayn Rand', pages:1200, price: 18.81)
+Book.where(title:"Ender's Game").first_or_create!(title:"Ender's Game", author:'Orson Scott Card', pages:352, price:4.83)
+Book.where(title:'Hamlet').first_or_create!(title:'Hamlet', author:'Shakespeare', pages:342, price:5.34)
