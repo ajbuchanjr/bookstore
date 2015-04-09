c750ccfb94313ab00808cda9f36a299ece54529e
Anthony Buchanan <ajbuchanjr@gmail.com>
2015-04-08 22:11:12 -0400

    added delete button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
-  def index
-    @available_at = Time.now
-    @books = Book.all
-  end
-  def show
-    @book = Book.find(params[:id])
-  end
+
+before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+
+def index
+  @available_at = Time.now
+  @books = Book.all
+end
+
+def show
+end
+
 def new
   @book = Book.new
 end
+
 def create
   @book = Book.new(book_params)
   @book.save
@@ -16,13 +21,16 @@ def create
 end
 
 def edit
-  @book = Book.find(params[:id])
 end
 
 def update
-  @book = Book.find(params[:id])
   @book.update(book_params)
   redirect_to @book
+end
+
+def destroy
+  @book.destroy
+  redirect_to books_url
 end
 
 private
@@ -30,4 +38,9 @@ private
 def book_params
   params.require(:book).permit(:title, :author, :pages, :price)
 end
+
+def set_book
+  @book = Book.find(params[:id])
+end
+
 end
