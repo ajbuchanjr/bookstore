e2167208f3438b383c012919b35bce8c23b0362b
ajbuchanjr <ajbuchanjr@gmail.com>
2015-04-15 22:27:56 -0400

    finished

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ before_action :set_book, only: [ :show, :edit, :update, :destroy ]
 
 def index
   @available_at = Time.now
-  @books = Book.all
+  @books = Book.order(:title).page(params[:page])
 end
 
 def show
